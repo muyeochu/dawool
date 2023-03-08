4c6982c0d7fcb396ad42c3122bc822482b6e855c
jun <ktf1406@naver.com>
2023-03-08 17:40:57 +0900

    feat : 카카로 로그인 구현

--- backend/api-server/src/main/java/com/dawool/api/controller/UserController.java
+++ backend/api-server/src/main/java/com/dawool/api/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.dawool.api.controller;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
+
+
+    // 카카오 로그인
+    @GetMapping("/kakao")
+    public String kakaoLogin(@RequestParam("code") String code) {
+        return null;
+    }
 }
