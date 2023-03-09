14eec4d42dd66650db5a997a98d5814ffe522bbf
haesom <fliamento@naver.com>
2023-03-09 18:03:42 +0900

    feat: header 작업

--- frontend/src/components/search/SearchList.tsx
+++ frontend/src/components/search/SearchList.tsx
@@ -1,9 +1,6 @@
-// props의 타입을 지정해줌
-interface WordProps {
-  word: string;
-}
+import { WordTypes } from "../../types/wordTypes";
 
-const SearchList = ({ word }: WordProps) => {
+const SearchList = ({ word }: WordTypes) => {
   return <h2>{word}에 대한 검색 결과입니다.</h2>;
 };
 
---
+++ frontend/src/types/wordTypes.ts
@@ -0,0 +1,3 @@
+export interface WordTypes {
+  word: string;
+}
