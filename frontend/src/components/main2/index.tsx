ca6c47d82f6d5dc8a5f7b1cc2782cb34e162afa3
haesom <fliamento@naver.com>
2023-03-09 09:24:05 +0900

    feat: Header 생성

---
+++ frontend/src/components/main2/index.tsx
@@ -0,0 +1,12 @@
+import { LandingContainer } from "./styles";
+
+const MainSecond = () => {
+  return (
+    <div>
+      <h2>두번째 메인페이지</h2>
+      <LandingContainer></LandingContainer>
+    </div>
+  );
+};
+
+export default MainSecond;
