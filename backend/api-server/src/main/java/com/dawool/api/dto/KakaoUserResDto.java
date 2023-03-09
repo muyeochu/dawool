bb87e9a35f06bf80f87bc2ab77b7c46b396f4f30
jun <ktf1406@naver.com>
2023-03-09 17:57:17 +0900

    feat : 카카오 유저 DTO

---
+++ backend/api-server/src/main/java/com/dawool/api/dto/KakaoUserResDto.java
@@ -0,0 +1,20 @@
+package com.dawool.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KakaoUserResDto {
+
+    private String nickName;
+    private String email;
+    private int ageRange;
+    private String gender;
+}
