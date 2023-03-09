072bfb483416b422835029e879206f6cdf533aff
lj05117 <lj05117@naver.com>
2023-03-09 10:29:06 +0900

    feat : footer 생성

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import Footer from "./components/common/Footer";
 
 ReactDOM.render(
   <RecoilRoot>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Footer/>
   </RecoilRoot>,
   document.getElementById("root")
 );
