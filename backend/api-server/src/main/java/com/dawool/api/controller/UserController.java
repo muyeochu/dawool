8a150a08650c935dbe147aa86f9f1b9dd5e97a62
jun <ktf1406@naver.com>
2023-03-09 17:57:01 +0900

    feat : 카카오 로그인 컨트롤러

--- backend/api-server/src/main/java/com/dawool/api/controller/UserController.java
+++ backend/api-server/src/main/java/com/dawool/api/controller/UserController.java
@@ -1,23 +1,33 @@
 package com.dawool.api.controller;
 
 
+import com.dawool.api.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
 
-
+    @Autowired
+    private UserService userService;
     // 카카오 로그인
     @GetMapping("/kakao")
-    public String kakaoLogin(@RequestParam("code") String code) {
-        return null;
+    public Map<String, String> kakaoLogin(@RequestParam("code") String code) {
+        Map<String, String> result = userService.getKakaoAccessToken(code);
+        return result;
+    }
+
+    @GetMapping("/kakao/userinfo")
+    public void userInfo() {
+        System.out.println("KAKAO INFO");
+        userService.getUserInfoByToken();
     }
 }
