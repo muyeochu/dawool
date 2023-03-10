9454a85c02f61cf4bde4d8b595d65cd0bc990b71
haesom <fliamento@naver.com>
2023-03-10 15:46:56 +0900

    feat: footer 넣기

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
-import Footer from "./components/common/Footer";
+import Footer from "./components/common/Footer/index";
 
 ReactDOM.render(
   <RecoilRoot>
     <App />
+    <Footer />
   </RecoilRoot>,
   document.getElementById("root")
 );
--- frontend/src/styles/appStyles.ts
+++ frontend/src/styles/appStyles.ts
@@ -4,3 +4,7 @@ export const GridContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 6fr 1fr;
 `;
+
+export const AppContainer = styled.div``;
+
+export const FooterStyle = styled.footer``;
