78896951f35934726993a6fac7cf19837174beb4
jun <ktf1406@naver.com>
2023-03-10 17:02:59 +0900

    feat : 카카오 유저 정보

--- backend/api-server/src/main/java/com/dawool/api/entity/User.java
+++ backend/api-server/src/main/java/com/dawool/api/entity/User.java
@@ -1,13 +1,27 @@
 package com.dawool.api.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document("user")
 public class User {
     @Id
     private String id;
+
+    private Long kakaoId;
     private String nickName;
     private String email;
-
+    private int ageRange;
+    private String gender;
 }
