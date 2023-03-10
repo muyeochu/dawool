eb3f36f3e88fdd5435d45ecab7f4dbbc1810ddcf
haesom <fliamento@naver.com>
2023-03-10 13:52:03 +0900

    feat: Header UI 편집

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,25 +13,28 @@ import {
   TourSpotPage,
 } from "./pages/index";
 import Header from "./components/common/Header";
+import { GridContainer } from "./styles/appStyles";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<IntroPage />} />
-          <Route path="/detail" element={<DetailPage />} />
-          <Route path="/interest" element={<InterestPage />} />
-          <Route path="/accommodation" element={<AccommodationPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/mycourse" element={<MyCoursePage />} />
-          <Route path="/restaurant" element={<RestaurantPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/survey" element={<SurveyPage />} />
-          <Route path="/tourspot" element={<TourSpotPage />} />
-        </Routes>
-      </BrowserRouter>
+      <GridContainer>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<IntroPage />} />
+            <Route path="/detail" element={<DetailPage />} />
+            <Route path="/interest" element={<InterestPage />} />
+            <Route path="/accommodation" element={<AccommodationPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/mycourse" element={<MyCoursePage />} />
+            <Route path="/restaurant" element={<RestaurantPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/survey" element={<SurveyPage />} />
+            <Route path="/tourspot" element={<TourSpotPage />} />
+          </Routes>
+        </BrowserRouter>
+      </GridContainer>
     </div>
   );
 }
