fedec98df1a2396a3d78bb4d7958ef6dd6ceecbd
lj05117 <lj05117@naver.com>
2023-03-13 13:31:50 +0900

    feat: footer 나오게 코드 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,32 +13,41 @@ import {
   TourSpotPage,
 } from "./pages/index";
 import Header from "./components/common/Header";
-import { GridContainer } from "./styles/appStyles";
 import { Waiting } from "./pages/LoginPage/waiting";
+import {
+  AppContainer,
+  GridContainer,
+  InvisibleBox,
+  BoxMainContainer,
+} from "./styles/appStyles";
 
 function App() {
   return (
-    <div>
+    <AppContainer>
       <GridContainer>
         <BrowserRouter>
           <Header />
-          <Routes>
-            <Route path="/" element={<IntroPage />} />
-            <Route path="/detail" element={<DetailPage />} />
-            <Route path="/interest" element={<InterestPage />} />
-            <Route path="/accommodation" element={<AccommodationPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/mycourse" element={<MyCoursePage />} />
-            <Route path="/restaurant" element={<RestaurantPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/survey" element={<SurveyPage />} />
-            <Route path="/tourspot" element={<TourSpotPage />} />
-            <Route path="/waiting" element={<Waiting/>}/>
-          </Routes>
+          <BoxMainContainer>
+            <InvisibleBox />
+            <Routes>
+              <Route path="/" element={<IntroPage />} />
+              <Route path="/detail" element={<DetailPage />} />
+              <Route path="/interest" element={<InterestPage />} />
+              <Route path="/accommodation" element={<AccommodationPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/mycourse" element={<MyCoursePage />} />
+              <Route path="/restaurant" element={<RestaurantPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/survey" element={<SurveyPage />} />
+              <Route path="/tourspot" element={<TourSpotPage />} />
+              <Route path="/waiting" element={<Waiting/>}/>
+            </Routes>
+          </BoxMainContainer>
         </BrowserRouter>
       </GridContainer>
-    </div>
+    </AppContainer>
   );
 }
+
 
 export default App;
