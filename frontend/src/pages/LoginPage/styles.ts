62bcf696a5e30a7a5f38fc584488633923db4ea5
lj05117 <lj05117@naver.com>
2023-03-13 10:39:44 +0900

    design: 로그인 화면 디자인

---
+++ frontend/src/pages/LoginPage/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+import {ReactComponent as BackImg} from "../../assets/images/BackImg.svg";
+
+export const BackgroundContainer = styled(BackImg)`
+    width:100%;
+    height:100%;
+`
+
+export const LoginText = styled.h1`
+    text-align:center;
+`
+
+
