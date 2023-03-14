9648ffd6e1f7b5baff83bf02997f4e9198bc49c4
jun <ktf1406@naver.com>
2023-03-14 09:55:52 +0900

    feat : WebConfig.java

--- backend/api-server/src/main/java/com/dawool/api/config/WebConfig.java
+++ backend/api-server/src/main/java/com/dawool/api/config/WebConfig.java
@@ -1,4 +1,23 @@
 package com.dawool.api.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+
+
+
+    }
 }
