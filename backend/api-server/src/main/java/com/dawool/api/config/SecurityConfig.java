032005263f0adf62d162f921736094c1019ecafa
jun <ktf1406@naver.com>
2023-03-14 17:24:04 +0900

    feat : Dto 생성

---
+++ backend/api-server/src/main/java/com/dawool/api/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.dawool.api.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
---
+++ backend/api-server/src/main/java/com/dawool/api/dto/TokenResDto.java
@@ -0,0 +1,19 @@
+package com.dawool.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenResDto {
+
+    private String nickName;
+    private String accessToken;
+    private String refreshToken;
+}
