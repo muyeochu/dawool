02e2333dda26eff5fe214392737bb44715325314
jun <ktf1406@naver.com>
2023-03-14 17:25:17 +0900

    feat : 유저 검색

--- backend/api-server/src/main/java/com/dawool/api/repository/UserRepository.java
+++ backend/api-server/src/main/java/com/dawool/api/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Boolean existsByKakaoId(long kakaoId);
+
+    User findByKakaoId(long kakaoId);
 }
