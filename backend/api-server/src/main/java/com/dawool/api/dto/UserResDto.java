369916bc32223cdcbff5263b3bebad4fda11a93a
jun <ktf1406@naver.com>
2023-03-14 17:25:29 +0900

    feat : Dto 수정

--- backend/api-server/src/main/java/com/dawool/api/dto/UserResDto.java
+++ backend/api-server/src/main/java/com/dawool/api/dto/UserResDto.java
@@ -1,19 +1,4 @@
 package com.dawool.api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class UserResDto {
-
-    private String nickName;
-    private String accessToken;
-    private String refreshToken;
 }
