51781e4fb2e9978645b79ae21cdf87a4036ae227
yyyrin <qsc130@gmail.com>
2023-03-15 02:17:38 +0900

    design: button 모듈화 작업 완료
    Former-commit-id: 75278f5e910d1978fea47e0ee9ab64d1d2327603

--- frontend/src/components/common/Button/index.tsx
+++ frontend/src/components/common/Button/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { StyledButton, ButtonText, ButtonImage } from "./styles";
+import React, { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { StyledButton, ButtonText, ButtonIcon } from "./styles";
+import { buttonState } from "../../../recoil/ButtonState";
 
 // children -> 하나의 자식 노드만 전달
 interface ButtonProps {
@@ -9,11 +11,31 @@ interface ButtonProps {
   imageSrc?: string;
 }
 
-export default function Button({ onClick, disabled, children, imageSrc }: ButtonProps) {
+export default function Button({ children, imageSrc }: ButtonProps) {
+  // 'useState' hook -> 클릭여부 상태값 관리 (기본값 'false')
+  const [isClicked, setIsClicked] = useState(false);
+
+  // 'useRecoilState' hook -> Recoil의 'buttonState' 값을 가져와 버튼 상태에 대한 정보 유지
+  const [button, setButton] = useRecoilState(buttonState);
+
+  // 'useEffect' hook -> 'clicked' 값이 변경될 때마다 Recoil의 `buttonState` update
+  // 'clicked'값이 'true'이면 Recoil 상태값 'buttonState'의 'clicked' 프로퍼티 'true'로 설정
+  useEffect(() => {
+    setButton((prevState) => ({ ...prevState, isClicked }));
+  }, [isClicked, setButton]);
+
+  // 클릭 이벤트가 발생-> handleClick 함수가 호출, clicked 상태값이 반전
+  function handleClick() {
+    setIsClicked(!isClicked);
+  }
+
   return (
-    <StyledButton onClick={onClick} disabled={disabled}>
-      {imageSrc && <ButtonImage src={imageSrc} />}
-      <ButtonText>{children}</ButtonText>
+    <StyledButton onClick={handleClick} isClicked={isClicked}>
+      {/* icon color 바뀌지가 않음.. 이거 해결해야함 */}
+      <ButtonIcon src={imageSrc} isClicked={isClicked} />
+      <ButtonText isClicked={isClicked}>
+        {children}
+      </ButtonText>
     </StyledButton>
   );
 }
--- frontend/src/pages/TourSpotPage.tsx
+++ frontend/src/pages/TourSpotPage.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { RecoilRoot } from "recoil";
 import Button from '../components/common/Button/index';
-import logo from '../assets/icon/eyeIc.svg';
+// import {ReactComponent as EyeIc} from '../assets/icon/eyeIc.svg';
+import eyeIc from '../assets/icon/eyeIc.svg';
 
 const TourSpotPage = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <div>
       <h1>관광지 페이지입니다.</h1>
       <h1>관광지 페이지입니다.</h1>
-      <Button onClick={() => alert("with image clicked")} imageSrc={logo}>
+      <Button onClick={handleClick} imageSrc={eyeIc}>
         시각장애
       </Button>
     </div>
