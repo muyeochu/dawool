0b66e27db722309d16ba43c0bfc469d956e6c758
yyyrin <qsc130@gmail.com>
2023-03-15 03:26:47 +0900

    design: 무장애 필터링 버튼 5개 구현
    Former-commit-id: 53f327924d9b1ff7bd7a03d4e1f2c2911da713db

--- frontend/src/pages/TourSpotPage.tsx
+++ frontend/src/pages/TourSpotPage.tsx
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
 import { RecoilRoot } from "recoil";
 import Button from '../components/common/Button/index';
-// import {ReactComponent as EyeIc} from '../assets/icon/eyeIc.svg';
+
+// icon
 import eyeIc from '../assets/icon/eyeIc.svg';
+import bathchairIc from '../assets/icon/bathchairIc.svg';
+import earIc from '../assets/icon/earIc.svg';
+import oldmanIc from '../assets/icon/oldmanIc.svg';
+import toddlerIc from '../assets/icon/toddlerIc.svg';
+
 
 const TourSpotPage = () => {
   const [isClicked, setIsClicked] = useState(false);
@@ -15,9 +21,24 @@ const TourSpotPage = () => {
     <div>
       <h1>관광지 페이지입니다.</h1>
       <h1>관광지 페이지입니다.</h1>
+
+      {/* 무장애 필터 button */}
+      <Button onClick={handleClick} imageSrc={bathchairIc}>
+        지체장애
+      </Button>
       <Button onClick={handleClick} imageSrc={eyeIc}>
         시각장애
       </Button>
+      <Button onClick={handleClick} imageSrc={earIc}>
+        청각장애
+      </Button>
+      <Button onClick={handleClick} imageSrc={oldmanIc}>
+        노인
+      </Button>
+      <Button onClick={handleClick} imageSrc={toddlerIc}>
+        영유아
+      </Button>
+
     </div>
   );
 };
