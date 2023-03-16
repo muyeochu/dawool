51cb4572386ef604ecbdfcd8bb76d4b9dfd81818
jungeun <kjjee99@naver.com>
2023-03-16 23:13:29 +0900

    feat: 관광지 목록 검색
    - 페이지네이션 완료
    
    
    Former-commit-id: 9acd256381750a4af499c74616d0c275ba2f99c7

--- backend/api-server/src/main/java/com/dawool/api/controller/PlaceController.java
+++ backend/api-server/src/main/java/com/dawool/api/controller/PlaceController.java
@@ -3,10 +3,12 @@ package com.dawool.api.controller;
 import com.dawool.api.dto.detailInfo.EntertainmentDto;
 import com.dawool.api.service.PlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,8 +22,8 @@ public class PlaceController {
     private final PlaceService placeService;
 
     @GetMapping("/list/spot")
-    public ResponseEntity<?> getEntertainmentList(){
-        List<EntertainmentDto> entertainmentList = placeService.getEntertainmentList();
+    public ResponseEntity<?> getEntertainmentList(Pageable pageable){
+        List<EntertainmentDto> entertainmentList = placeService.getEntertainmentList(pageable);
 
         Map<String, List<EntertainmentDto>> response = new HashMap<>();
         response.put("contents", entertainmentList);
--- backend/api-server/src/main/java/com/dawool/api/service/PlaceService.java
+++ backend/api-server/src/main/java/com/dawool/api/service/PlaceService.java
@@ -1,19 +1,27 @@
 package com.dawool.api.service;
 
 import com.dawool.api.dto.detailInfo.EntertainmentDto;
+import com.dawool.api.entity.Entertainment;
+import com.dawool.api.repository.EntertainmentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PlaceService {
 
-    private final EntertainmentDto entertainmentDto;
+    private final EntertainmentRepository entertainmentRepository;
 
-    public List<EntertainmentDto> getEntertainmentList(){
-
+    public List<EntertainmentDto> getEntertainmentList(Pageable pageable){
+        List<Entertainment> entertainment = entertainmentRepository.findAll(pageable).toList();
+        System.out.println(entertainment.get(0).getContentid());
 
         return null;
     }
