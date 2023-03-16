47c26af009a695daf8ee30ea8331b4416c546b66
haesom <fliamento@naver.com>
2023-03-16 17:48:50 +0900

    feat: survey용 modal 구현 완료
    Former-commit-id: e92f8552ce974936a8d1db5c5c8cf40549519221

--- frontend/src/pages/AccommodationPage.tsx
+++ frontend/src/pages/AccommodationPage.tsx
@@ -1,28 +1,6 @@
-import useModal from "../components/utils/useModal";
-
 const AccommodationPage = () => {
-  const { openModal, closeModal } = useModal();
-
-  const modalData = {
-    title: "타이틀",
-    content: "모달 내용",
-    callback: () => {
-      alert("삭제되었습니다!");
-      closeModal();
-      // 삭제 후 목록을 다시 불러오는 함수 작성
-    },
-  };
-
   return (
     <div>
-      <button
-        onClick={() => {
-          openModal(modalData);
-        }}
-      >
-        모달창 테스트
-      </button>
-      <h1>숙박 목록 페이지입니다.</h1>
       <h1>숙박 목록 페이지입니다.</h1>
       <h1>숙박 목록 페이지입니다.</h1>
       <h1>숙박 목록 페이지입니다.</h1>
