9b967668115664cf3188ab171c2e3bc5f50b7fca
jungeun <kjjee99@naver.com>
2023-03-16 23:13:07 +0900

    refac: 필드 타입 수정
    - DB 타입에 맞게 수정
    
    
    Former-commit-id: 0fd835ab58048444a32a3293e9b6bb28ac2eaff7

--- backend/api-server/src/main/java/com/dawool/api/entity/CommonInfo.java
+++ backend/api-server/src/main/java/com/dawool/api/entity/CommonInfo.java
@@ -1,7 +1,11 @@
 package com.dawool.api.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+@Getter
+@Setter
 public class CommonInfo {
 
     @Id
@@ -11,7 +15,7 @@ public class CommonInfo {
     private int contentid;
     // 관광지 타입
     // 12: 관광지 14: 문화시설 15: 축제공연 25: 여행코스 28: 레포츠 32: 숙박 38: 쇼핑 39: 음식점
-//    private int contenttypeid;
+    private int contenttypeid;
     private String title;
     // 소분류
     private String category;
@@ -26,11 +30,21 @@ public class CommonInfo {
     private String addr1;
     private String addr2;
     // 경도
-    private int mapx;
+    private float mapx;
     // 위도
-    private int mapy;
+    private float mapy;
     // 지도 레벨
-    private int mlevel;
+    private float mlevel;
+    // 청각장애
+    private int deaf;
+    // 시각장애
+    private int visually_impaired;
+    // 지체장애
+    private int mobility_weak;
+    // 노인
+    private int old;
+    // 영유아
+    private int infant;
     // 조회수
     private int hit;
 }
--- backend/api-server/src/main/java/com/dawool/api/entity/Entertainment.java
+++ backend/api-server/src/main/java/com/dawool/api/entity/Entertainment.java
@@ -1,24 +1,28 @@
 package com.dawool.api.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Entertainment")
+@Getter
+@Setter
+@Document(collection = "entertainment")
 public class Entertainment extends CommonInfo {
 
     // 유모차 대여 가능 여부
-    private boolean chkbabycarriage;
+    private int chkbabycarriage;
     // 애완동물 동반 여부
-    private boolean chkpet;
+    private int chkpet;
     // 체험 가능 연령
     private String expagerange;
     // 체험 안내
     private String expguide;
     // 세계문화유산유무
-    private boolean heritage1;
+    private float heritage1;
     // 세계자연유산유무
-    private boolean heritage2;
+    private float heritage2;
     // 세계기록유산유무
-    private boolean heritage3;
+    private float heritage3;
     // 문의 및 안내
     private String infocenter;
     // 주차시설
