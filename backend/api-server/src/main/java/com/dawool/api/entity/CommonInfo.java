da4fd3042c82cfbe247112fd9e74acf24e04c2e0
jungeun <jegarnet23@gmail.com>
2023-03-16 15:18:28 +0900

    feat: 공통정보 Entity 생성
    Former-commit-id: 8b32da0db49df8bbad97d107e34ea66178e2db0e

---
+++ backend/api-server/src/main/java/com/dawool/api/entity/CommonInfo.java
@@ -0,0 +1,36 @@
+package com.dawool.api.entity;
+
+import org.springframework.data.annotation.Id;
+
+public class CommonInfo {
+
+    @Id
+    private String id;
+
+    // 관광지의 고유번호
+    private int contentid;
+    // 관광지 타입
+    // 12: 관광지 14: 문화시설 15: 축제공연 25: 여행코스 28: 레포츠 32: 숙박 38: 쇼핑 39: 음식점
+//    private int contenttypeid;
+    private String title;
+    // 소분류
+    private String category;
+    // URL
+    private String homepage;
+    // 대표 이미지
+    private String firstimage;
+    private String firstimage2;
+    // 지역 코드(특별시/광역시/도)
+    private int areacode;
+    // 도로명 주소
+    private String addr1;
+    private String addr2;
+    // 경도
+    private int mapx;
+    // 위도
+    private int mapy;
+    // 지도 레벨
+    private int mlevel;
+    // 조회수
+    private int hit;
+}
