91abc65fb1283571ab3944e5efc2cd7b96e080fd
jun <ktf1406@naver.com>
2023-03-17 18:00:30 +0900

    feat : 검색 컨트롤러 추가
    Former-commit-id: 39b6b87ad54c3391507b59e87062f11efc3aba3f

---
+++ backend/api-server/src/main/java/com/dawool/api/controller/SearchController.java
@@ -0,0 +1,35 @@
+package com.dawool.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 검색 controller
+ *
+ * @author 이준
+ */
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/search")
+public class SearchController {
+
+    /**
+     * 지역 검색
+     *
+     * @param areaCode
+     * @param pageable
+     * @return
+     */
+    @GetMapping("/")
+    public ResponseEntity<?> searchRegion(@RequestParam("area") String area, Pageable pageable) {
+
+        return null;
+    }
+}
