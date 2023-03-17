8a2da03c0672cdbe3d3452fae124f2f9d5ce98cf
jungeun <jegarnet23@gmail.com>
2023-03-17 13:33:01 +0900

    feat: 지역별 관광지 검색
    Former-commit-id: 9fbdafddee4eeff6bb51a456b400045b99387d7c

--- backend/api-server/src/main/java/com/dawool/api/repository/EntertainmentRepository.java
+++ backend/api-server/src/main/java/com/dawool/api/repository/EntertainmentRepository.java
@@ -1,10 +1,14 @@
 package com.dawool.api.repository;
 
 import com.dawool.api.entity.Entertainment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EntertainmentRepository extends MongoRepository<Entertainment, String> {
 
+    List<Entertainment> findByAreacode(String areaCode, Pageable pageable);
 }
