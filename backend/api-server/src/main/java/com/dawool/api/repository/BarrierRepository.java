ca2ef7228c100db8a81a738f1be470cdaf465bc7
jungeun <kjjee99@naver.com>
2023-03-18 20:27:24 +0900

    chore: JavaDoc 작성
    Former-commit-id: 97dc3bf3911888a6ecd2e0c38078310d58d36d1e

--- backend/api-server/src/main/java/com/dawool/api/repository/BarrierRepository.java
+++ backend/api-server/src/main/java/com/dawool/api/repository/BarrierRepository.java
@@ -4,6 +4,11 @@ import com.dawool.api.entity.Barrier;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 무장애 정보 Repository
+ *
+ * @author 김정은
+ */
 @Repository
 public interface BarrierRepository extends MongoRepository<Barrier, String> {
 
