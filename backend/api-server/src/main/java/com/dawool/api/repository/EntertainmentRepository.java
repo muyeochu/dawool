cfadb08d75ea70f66c3ee7aa0a82aee88efa9b6c
jungeun <kjjee99@naver.com>
2023-03-18 20:33:16 +0900

    chore: 오탈자 수정
    Former-commit-id: 22675b6f2da3af2ba1a50a3750c60d11dfa50b14

--- backend/api-server/src/main/java/com/dawool/api/repository/EntertainmentRepository.java
+++ backend/api-server/src/main/java/com/dawool/api/repository/EntertainmentRepository.java
@@ -24,7 +24,7 @@ public interface EntertainmentRepository extends MongoRepository<Entertainment, 
     List<Entertainment> findByAreacode(String areaCode);
 
     /**
-     * 지역 별 청각쟝애인 시설있는 관광지 목록
+     * 지역 별 청각장애인 시설있는 관광지 목록
      *
      * @param areaCode
      * @param Deaf
