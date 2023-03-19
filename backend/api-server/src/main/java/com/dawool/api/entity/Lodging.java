9198ef35f863defcdbb9fc3cfa36cd77af01afdd
jungeun <kjjee99@naver.com>
2023-03-19 16:59:01 +0900

    feat: 숙박 Entity
    Former-commit-id: ad13eeb13ad3b4fb3fb17f05acf8f94d9292b865

---
+++ backend/api-server/src/main/java/com/dawool/api/entity/Lodging.java
@@ -0,0 +1,53 @@
+package com.dawool.api.entity;
+
+/**
+ * 숙박(32) 관련 정보
+ *
+ * @author 김정은
+ */
+public class Lodging extends CommonInfo{
+
+    // 입실시간
+    private String checkintime;
+    // 퇴실시간
+    private String checkouttime;
+    // 취사 여부
+    private String chkcooking;
+    // 식음료장
+    private String foodplace;
+    // 한옥 여부
+    private float hanok;
+    // 문의 및 안내
+    private String infocenterlodging;
+    // 주차시설
+    private String parkinglodging;
+    // 픽업서비스
+    private String pickup;
+    // 예약 홈페이지
+    private String reservationurl;
+    // 부대시설
+    private String subfacility;
+    // 바베큐장 여부
+    private float barbecue;
+    // 피트니스 여부
+    private float fitness;
+    // 음료장 여부
+    private float beverage;
+    // 자전거 대여 여부
+    private float bicycle;
+    // 캠프파이어 여부
+    private float campfire;
+    // 노래방 여부
+    private float karaoke;
+    // 공용 샤워실 여부
+    private float publicbath;
+    // 공용 PC방 여부
+    private float publicpc;
+    // 사우나실
+    private float sauna;
+    // 스포츠센터
+    private float sports;
+    // 환불규정
+    private String refundregulation;
+
+}
