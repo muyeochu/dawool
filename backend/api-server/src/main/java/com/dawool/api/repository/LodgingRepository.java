8efed8a046edd45bcd27ecd13438fbe842d10696
jungeun <kjjee99@naver.com>
2023-03-19 16:59:18 +0900

    feat: 숙박 Repository
    Former-commit-id: 3ec100b6b501c47bd4e44f33399589f3ca90577b

---
+++ backend/api-server/src/main/java/com/dawool/api/repository/LodgingRepository.java
@@ -0,0 +1,27 @@
+package com.dawool.api.repository;
+
+import com.dawool.api.entity.Lodging;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LodgingRepository extends MongoRepository<Lodging, String> {
+    /**
+     * 지역 별 관광지 목록
+     *
+     * @param areaCode
+     * @return
+     */
+    List<Lodging> findByAreacode(String areaCode);
+
+    /**
+     * 지역 별 청각장애인 시설있는 숙박 목록
+     *
+     * @param areaCode
+     * @param Deaf
+     * @return
+     */
+    List<Lodging> findByAreacodeAndDeaf(String areaCode, String Deaf);
+}
