929bf4adc83387fbf7b1525bcaac45ce41280dca
jungeun <kjjee99@naver.com>
2023-03-19 18:45:09 +0900

    refac: Annotation 추가
    Former-commit-id: b96036991286e53103b676ecef9ffd6dbe665c37

--- backend/api-server/src/main/java/com/dawool/api/dto/detailInfo/LodgingDto.java
+++ backend/api-server/src/main/java/com/dawool/api/dto/detailInfo/LodgingDto.java
@@ -1,53 +1,144 @@
 package com.dawool.api.dto.detailInfo;
 
+import com.dawool.api.code.Category;
+import com.dawool.api.entity.Barrier;
+import com.dawool.api.entity.Lodging;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * 숙박(32) 관련 정보
  *
  * @author 김정은
  */
-public class LodgingDto {
+@Getter
+@Setter
+@NoArgsConstructor
+public class LodgingDto extends CommonInfoDto{
 
     // 입실시간
-    private String checkintime;
+    private String checkInTime;
     // 퇴실시간
-    private String checkouttime;
+    private String checkOutTime;
     // 취사 여부
-    private String chkcooking;
+    private String isCooking;
     // 식음료장
-    private String foodplace;
+    private String foodPlace;
     // 한옥 여부
-    private boolean hanok;
+    private float hanok;
     // 문의 및 안내
-    private String infocenterlodging;
+    private String infoCenter;
     // 주차시설
-    private String parkinglodging;
+    private String commonParking;
     // 픽업서비스
     private String pickup;
     // 예약 홈페이지
-    private String reservationurl;
+    private String reservationUrl;
     // 부대시설
-    private String subfacility;
+    private String subFacility;
     // 바베큐장 여부
-    private boolean barbecue;
+    private float barbecue;
     // 피트니스 여부
-    private boolean fitness;
+    private float fitness;
     // 음료장 여부
-    private boolean beverage;
+    private float beverage;
     // 자전거 대여 여부
-    private boolean bicycle;
+    private float bicycle;
     // 캠프파이어 여부
-    private boolean campfire;
+    private float campfire;
     // 노래방 여부
-    private boolean karaoke;
+    private float karaoke;
     // 공용 샤워실 여부
-    private boolean publicbath;
+    private float publicBath;
     // 공용 PC방 여부
-    private boolean publicpc;
+    private float publicPc;
     // 사우나실
-    private boolean sauna;
+    private float sauna;
     // 스포츠센터
-    private boolean sports;
+    private float sports;
     // 환불규정
-    private String refundregulation;
+    private String refundRegulation;
 
+    @Builder
+    public LodgingDto(int contentId, String title, String category, String homepage, String firstImage, String firstImage2,
+                      int areaCode, String addr1, String addr2, float mapX, float mapY, float mLevel,
+                      int deaf, int visuallyImpaired, int mobilityWeak, int old, int infant, boolean isLiked, int hit, BarrierDto barrier,
+                      String checkInTime, String checkOutTime, String isCooking, String foodPlace, float hanok,
+                      String infoCenter, String commonParking, String pickup, String reservationUrl,
+                      String subFacility, float barbecue, float fitness, float beverage, float bicycle, float campfire, float karaoke, float publicBath, float publicPc, float sauna, float sports, String refundRegulation) {
+        super(contentId, title, category, homepage, firstImage, firstImage2, areaCode, addr1, addr2, mapX, mapY, mLevel, deaf, visuallyImpaired, mobilityWeak, old, infant, isLiked, hit, barrier);
+        this.checkInTime = checkInTime;
+        this.checkOutTime = checkOutTime;
+        this.isCooking = isCooking;
+        this.foodPlace = foodPlace;
+        this.hanok = hanok;
+        this.infoCenter = infoCenter;
+        this.commonParking = commonParking;
+        this.pickup = pickup;
+        this.reservationUrl = reservationUrl;
+        this.subFacility = subFacility;
+        this.barbecue = barbecue;
+        this.fitness = fitness;
+        this.beverage = beverage;
+        this.bicycle = bicycle;
+        this.campfire = campfire;
+        this.karaoke = karaoke;
+        this.publicBath = publicBath;
+        this.publicPc = publicPc;
+        this.sauna = sauna;
+        this.sports = sports;
+        this.refundRegulation = refundRegulation;
+    }
+
+    /**
+     * Entity -> DTO
+     *
+     * @param lodging
+     * @param barrier
+     * @return
+     */
+    public LodgingDto of(Lodging lodging, Barrier barrier) {
+        return LodgingDto.builder()
+                // 공통정보
+                .contentId(lodging.getContentid())
+                .addr1(lodging.getAddr1())
+                .title(lodging.getTitle())
+                .category(Category.valueOf(lodging.getCat3()).getCategory())
+                .deaf(lodging.getDeaf())
+                .visuallyImpaired(lodging.getVisually_impaired())
+                .mobilityWeak(lodging.getMobility_weak())
+                .old(lodging.getOld())
+                .infant(lodging.getInfant())
+                .isLiked(false)
+                .firstImage(lodging.getFirstimage())
+                .homepage(lodging.getHomepage())
+                .mapX(lodging.getMapx())
+                .mapY(lodging.getMapy())
+                .mLevel(lodging.getMlevel())
+                .barrier(new BarrierDto().of(barrier))
+                // 상세정보
+                .checkInTime(lodging.getCheckintime())
+                .checkOutTime(lodging.getCheckouttime())
+                .isCooking(lodging.getChkcooking())
+                .commonParking(lodging.getParkinglodging())
+                .infoCenter(lodging.getInfocenterlodging())
+                .fitness(lodging.getFitness())
+                .barbecue(lodging.getBarbecue())
+                .bicycle(lodging.getBicycle())
+                .beverage(lodging.getBeverage())
+                .campfire(lodging.getCampfire())
+                .hanok(lodging.getHanok())
+                .karaoke(lodging.getKaraoke())
+                .publicBath(lodging.getPublicbath())
+                .publicPc(lodging.getPublicpc())
+                .reservationUrl(lodging.getReservationurl())
+                .refundRegulation(lodging.getRefundregulation())
+                .sauna(lodging.getSauna())
+                .sports(lodging.getSports())
+                .subFacility(lodging.getSubfacility())
+                .pickup(lodging.getPickup())
+                .build();
+    }
 }
