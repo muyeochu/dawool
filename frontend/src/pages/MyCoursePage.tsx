4d5c2049733d52be86e5574cb12cc87a3a9f6056
lj05117 <lj05117@naver.com>
2023-03-20 17:29:16 +0900

    feat: 카카오 지도 컴포넌트 불러오기
    Former-commit-id: 3ca03c2c09cd853242bdaf836895980f6803f1e2

--- frontend/src/pages/MyCoursePage.tsx
+++ frontend/src/pages/MyCoursePage.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { white,mainColor } from "../styles/Colors";
 import { ReactComponent as MapIc } from "../assets/icon/mapIc.svg";
+import KakaoMap from "../components/course/map";
 const CourseContainer=styled.div`
   grid-column: 1/4;
 `
@@ -27,7 +28,9 @@ const MyCoursePage = () => {
     <SideItem>
         <SideHeader><MapIc/> 내 코스 관리</SideHeader>
     </SideItem>
-    <MapItem></MapItem>
+    <MapItem>
+      <KakaoMap lat="33.450701" lng="126.570667" ></KakaoMap>
+    </MapItem>
     </CourseContainer>
   );
 };
