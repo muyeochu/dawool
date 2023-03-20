4ac8acf447335869647e639a9ac415dcc739ba75
yyyrin <qsc130@gmail.com>
2023-03-20 17:54:26 +0900

    design: dropdown ui 구현 중
    Former-commit-id: a5f6c0713dbdef5b9738df1153765671caa2e430

--- frontend/src/components/survey/Accordion/question2/index.tsx
+++ frontend/src/components/survey/Accordion/question2/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { HangjungdongContanier } from "./styles";
+import DropDown from './dropdown/index'
 
 const SecondQuestion = () => {
-  return <HangjungdongContanier>지도 넣을 자리!!</HangjungdongContanier>;
+  return <HangjungdongContanier>
+    <DropDown/>
+  </HangjungdongContanier>;
 };
 
 export default SecondQuestion;
