cd64b8ca2c2abeffd9833ed67636084ad9fd1825
jungeun <jegarnet23@gmail.com>
2023-03-20 16:33:40 +0900

    refac: collection 이름 수정
    Former-commit-id: 21fa43e16bc091a5d588b04c899587733feba4a5

--- backend/api-server/src/main/java/com/dawool/api/entity/Barrier.java
+++ backend/api-server/src/main/java/com/dawool/api/entity/Barrier.java
@@ -15,7 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
 @ToString
-@Document(collection = "barrier_free_store")
+@Document(collection = "barrier")
 public class Barrier {
     @Id
     private String id;
