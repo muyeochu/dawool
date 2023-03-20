baa8745be6d25f9ee0d6f3ce18083d19306b7c7e
lj05117 <lj05117@naver.com>
2023-03-20 10:29:40 +0900

    feat: 사이드바와 지도 다단, 높이 맞추기
    Former-commit-id: 53ecbeecbe4846c702ba2c7893bbe09b200bf6ab

--- frontend/src/pages/MyCoursePage.tsx
+++ frontend/src/pages/MyCoursePage.tsx
@@ -1,8 +1,27 @@
+import styled from "styled-components";
+
+const CourseContainer=styled.div`
+  grid-column: 1/4;
+`
+const SideItem = styled.div`
+  width: 27%;
+  height: 100vh;
+  float: left;
+`
+const MapItem = styled.div`
+  width: 73%;
+  height:100vh;
+  float: left;
+`
+
 const MyCoursePage = () => {
   return (
-    <div>
-      <h1>내 코스 관리 페이지입니다.</h1>
-    </div>
+    <CourseContainer>
+    <SideItem>
+        <h1>내 코스 관리</h1>
+    </SideItem>
+    <MapItem></MapItem>
+    </CourseContainer>
   );
 };
 
