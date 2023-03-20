435c70abcd87f0e28d6ca0aa28bfdce5c3e52fb0
jungeun <jegarnet23@gmail.com>
2023-03-20 14:05:06 +0900

    feat: 쇼핑 Entity 생성
    Former-commit-id: 1f46d257c8ca23b8a8631c5b0a128807454bc834

---
+++ backend/api-server/src/main/java/com/dawool/api/entity/Shopping.java
@@ -0,0 +1,40 @@
+package com.dawool.api.entity;
+
+import com.dawool.api.dto.detailInfo.CommonInfoDto;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 쇼핑(38) 관련 정보
+ *
+ * @author 김정은
+ */
+
+@Getter
+@Setter
+public class Shopping extends CommonInfo {
+
+    // 유모차 대여 여부
+    private int chkbabycarriageshopping;
+    // 신용카드 가능 여부
+    private String chkcreditcardshopping;
+    // 애완동물 동반 여부
+    private int chkpetshopping;
+    // 장서는 날
+    private String fairday;
+    // 문의 및 안내
+    private String infocentershopping;
+    // 영업시간
+    private String opentime;
+    // 주차시설
+    private String parkingshopping;
+    // 쉬는 날
+    private String restdateshopping;
+    // 화장실 시설
+    private String restroom;
+    // 판매 품목
+    private String saleitem;
+    // 매장 안내
+    private String shopguide;
+
+}
