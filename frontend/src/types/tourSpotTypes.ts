b583e026272044f37da632eaa654491ed9d431a2
haesom <fliamento@naver.com>
2023-03-21 17:51:15 +0900

    feat: 상세페이지 버튼 구현중
    Former-commit-id: 6f2b7255736e0efa5631816dbf024494d43856e1

---
+++ frontend/src/types/tourSpotTypes.ts
@@ -0,0 +1,16 @@
+export interface tourSpotTypes {
+  chkbabycarriage: string;
+  chkpet: string;
+  expagerange: string;
+  expguide: string;
+  heritage1: string;
+  heritage2: string;
+  heritage3: string;
+  infocenter: string;
+  parking: string;
+  restdate: string;
+  useseason: string;
+  usetime: string;
+}
+
+export interface CultureTypes {}
