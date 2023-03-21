afcf7925bd812137c482f5e43bbe77a3c3e59c33
jun <ktf1406@naver.com>
2023-03-21 17:51:37 +0900

    feat : 테스트
    Former-commit-id: bad5888cfdd69e8d0e649ed2d0b04ca437e83db3

--- backend/api-server/src/test/java/com/dawool/api/ApiServerApplicationTests.java
+++ backend/api-server/src/test/java/com/dawool/api/ApiServerApplicationTests.java
@@ -1,13 +1,50 @@
 package com.dawool.api;
 
+import com.dawool.api.entity.Shopping;
+import com.dawool.api.repository.CommonTemplate;
+import com.dawool.api.repository.RestaurantRepository;
+import com.dawool.api.repository.ShoppingRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class ApiServerApplicationTests {
 
+	@Autowired
+	private ShoppingRepository shoppingRepository;
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+	@Autowired
+	private CommonTemplate commonTemplate;
+	@Autowired
+	private MongoTemplate mongoTemplate;
 	@Test
 	void contextLoads() {
+		List<Shopping> list = null;
+//		list = shoppingRepository.findByTitleRegex(".*롯데.*");
+//
+//		for(Shopping l : list) {
+//			System.out.println("### Contains " + l.getTitle());
+//		}
+
+		String str = "10001";
+		String[] arr = str.split("");
+		System.out.println(Arrays.toString(arr));
+		list = mongoTemplate.find(commonTemplate.findByAreacodeAndBarrierFree(1, arr), Shopping.class,"shopping");
+		System.out.println(list.size());
+		if(!list.isEmpty()) {
+			for (Shopping l : list) {
+				System.out.println("### Filter : " + l.getTitle());
+			}
+		}
+		else {
+			System.out.println("###EMPTY###");
+		}
 	}
 
 }
