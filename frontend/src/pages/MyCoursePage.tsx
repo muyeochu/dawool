232dbc32544638aa9389062d48c89801078ef191
lj05117 <lj05117@naver.com>
2023-03-21 14:34:51 +0900

    feat: 컴포넌트 분리 및 추가
    Former-commit-id: fdf547a17d5cec23d6950dde8f1a7812543a6562

--- frontend/src/pages/MyCoursePage.tsx
+++ frontend/src/pages/MyCoursePage.tsx
@@ -1,36 +1,21 @@
-import styled from "styled-components";
-import { white,mainColor } from "../styles/Colors";
-import { ReactComponent as MapIc } from "../assets/icon/mapIc.svg";
+import {CourseContainer, SideItem, SideHeader, MapItem, SideHeaderText, MapIcStyle} from "../components/course/styles"
 import KakaoMap from "../components/course/map";
-const CourseContainer=styled.div`
-  grid-column: 1/4;
-`
-const SideItem = styled.div`
-  width: 27%;
-  height: 100vh;
-  float: left;
-  box-shadow: 10px 0px 20px rgba(0, 0, 0, 0.25);
-`
-const MapItem = styled.div`
-  width: 73%;
-  height:100vh;
-  float: left;
-`
-const SideHeader = styled.h1`
-  color:${white};
-  background-color:${mainColor};
-  text-align: center;
-`
+import MyCourseSideBar from "../components/course/sideBar/index"
 
 const MyCoursePage = () => {
   return (
     <CourseContainer>
-    <SideItem>
-        <SideHeader><MapIc/> 내 코스 관리</SideHeader>
-    </SideItem>
-    <MapItem>
-      <KakaoMap lat="33.450701" lng="126.570667" ></KakaoMap>
-    </MapItem>
+      <SideItem>
+          <SideHeader>
+            <SideHeaderText>
+              <MapIcStyle /> 내 코스 관리
+            </SideHeaderText>
+            <MyCourseSideBar/>
+          </SideHeader>
+      </SideItem>
+      <MapItem>
+        <KakaoMap lat="33.450701" lng="126.570667" ></KakaoMap>
+      </MapItem>
     </CourseContainer>
   );
 };
