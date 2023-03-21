70a5193b2228f962d385c45ad0b18e718922322c
lj05117 <lj05117@naver.com>
2023-03-21 14:31:40 +0900

    feat: 내 코스 관리 페이지 사이드바의 폴더 생성 헤더
    Former-commit-id: 205d5f63b9544aa484374f71f05b25eb4c1a6474

---
+++ frontend/src/components/course/sideBar/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import { white,black } from "../../../styles/Colors";
+import { SideHeader } from "../styles";
+
+export const SideInHeader = styled(SideHeader)`
+  background-color:${white};
+  height: 8vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+export const SideInHeaderText = styled.h4`
+  color:${black};
+  
+`
