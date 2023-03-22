3f3c7e9225743f5f17e9fc39f40dbd8e28137f6c
jun <ktf1406@naver.com>
2023-03-22 17:24:43 +0900

    feat : 키워드 검색 컨트롤러
    Former-commit-id: 5e6eb5ce55e86cee4ba4b9437aa528c7b40d84ea

--- backend/api-server/src/main/java/com/dawool/api/controller/SearchController.java
+++ backend/api-server/src/main/java/com/dawool/api/controller/SearchController.java
@@ -3,12 +3,15 @@ package com.dawool.api.controller;
 import com.dawool.api.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 검색 controller
@@ -21,22 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/search")
 public class SearchController {
 
-    private SearchService searchService;
+    private final SearchService searchService;
+
     /**
-     * 지역 검색
+     * 키워드 검색
      *
-     * @param areaCode
-     * @param pageable
+     * @param title
+     * @param type
+     * @param barrier
+     * @param page
+     * @param size
      * @return
      */
-    @GetMapping("/")
+    @GetMapping("")
     public ResponseEntity<?> searchRegion(
             @RequestParam("title") String title,
             @RequestParam("type") int type,
             @RequestParam("barrier") String barrier,
             int page, int size
     ) {
-        searchService.getSearchList(title, type, barrier, page, size);
-        return null;
+        List<?> searchList;
+        searchList = searchService.getSearchList(title, type, barrier, page, size);
+        Map<String, List<?>> response = new HashMap<>();
+        response.put("contents", searchList);
+        return ResponseEntity.ok(response);
     }
 }
