62d97d24bfaa8b3023073e82f7e297c0cc05e175
lj05117 <lj05117@naver.com>
2023-03-23 10:24:42 +0900

    feat: 배포 백 서버로 데이터 요청
    Former-commit-id: 9548a2c7a903623a2781df166d4b44a81f0a1dbf

--- frontend/src/components/auth/KakaoAuthHandle.tsx
+++ frontend/src/components/auth/KakaoAuthHandle.tsx
@@ -4,9 +4,6 @@ import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
 import {useSetRecoilState,useRecoilState } from "recoil";
 import {userState,User} from "../../recoil/UserState"
-const REST_API_KEY = `f9d7aca66b9d117f55a3c47a1f2f6eef`;
-const REDIRECT_URI = `http://localhost:3000/api/user/kakao/callback`;
-const KAKAO_AUTH_URL =`https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 const KakaoAuthHandle = ()=>{
   // const setUser = useSetRecoilState(userState);
@@ -19,7 +16,8 @@ const KakaoAuthHandle = ()=>{
   useEffect(()=>{
     let code = new URL(window.location.href).searchParams.get('code')
     const kakaoLogin = async()=>{
-      await axios.get(`http://localhost:8888/api/user/kakao/callback?code=${code}`)
+      // await axios.get(`http://localhost:8888/api/user/kakao/callback?code=${code}`)
+      await axios.get(`http://j8d105.p.ssafy.io:8888/api/user/kakao/callback?code=${code}`)
       .then((res)=>{
         console.log(res);
         localStorage.setItem("token",res.data.accessToken);
@@ -31,7 +29,6 @@ const KakaoAuthHandle = ()=>{
         console.log(user);
         navigate("/tourspot");
         //리코일에 데이터 담고 바로 메인페이지로 보내기
-
       })
     }
     kakaoLogin()
