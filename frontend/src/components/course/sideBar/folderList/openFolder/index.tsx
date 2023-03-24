2937190089ebdb89691f123a484292f765a2e1a8
lj05117 <lj05117@naver.com>
2023-03-24 15:45:43 +0900

    style: 폴더 삭제 UI
    Former-commit-id: 36984ad83bfbbb1c7662ed84614a5300cbfdc88c

--- frontend/src/components/course/sideBar/folderList/openFolder/index.tsx
+++ frontend/src/components/course/sideBar/folderList/openFolder/index.tsx
@@ -1,48 +1,37 @@
 import { useRecoilState } from "recoil";
-import styled from "styled-components"
 import {FolderState} from "../../../../../recoil/CourseFolderState"
-import {ExitFolderContainer,ExitFolderButton,ArrowIc,DotIc,FolderContainer} from "./styles"
+import {ExitFolderContainer,ExitFolderButton,ArrowIc,DeleteIc,FolderContainer} from "./styles"
 import { FolderYellowIc } from "../styles";
 import { useState, useRef } from "react";
-import { DropDownContainer,DropDownContent } from "../../../../common/Header/styles";
-import { grey } from "../../../../../styles/Colors";
+// import axios from "axios";
 
-export const DotContainer = styled.div`
-    z-index: 100;
-    position: absolute;
-  text-align: center;
-  overflow: hidden;
-  margin-left:20%;
-`
-export const DotContent = styled(DropDownContent)`
-    border: 1px solid ${grey[300]};
-`
+
 export const FolderInside=()=>{
-    const ref = useRef<HTMLDivElement>(null);
+    // const ref = useRef<HTMLDivElement>(null);
     const [folderState, setFolderState] = useRecoilState(FolderState);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     function exitFolder(){
         setFolderState({
             isOpen:false,
-            opendFolder:""
+            opendFolder:"",
+            courseId:1 //나중에contentid로 넣기
         });
     }
-    function dotOpen(){
-        setIsMenuOpen(!isMenuOpen);
+    function deleteNowFolder(){
+        //만들어놓은 alert창 띄우기
+        alert("임시")
+        //그 뒤에 진자 삭제하겠다고 누르면 아래 코드 실행
+        // const deleteNowFolder = async ()=>{
+        //   await axios
+        //   .delete(process.env.REACT_APP_API_BASE_URL+"/api/user/my-course/{courseId}")
+        // }
+
+        exitFolder();
     }
     return(<>
     <ExitFolderContainer >
     <ExitFolderButton onClick={exitFolder}><ArrowIc/>전체 코스 보기</ExitFolderButton>
     </ExitFolderContainer>
-    <FolderContainer><FolderYellowIc/>{folderState.opendFolder}<DotIc onClick={dotOpen}/></FolderContainer>
-    {isMenuOpen?(
-                <DotContainer ref={ref}>
-                  <DotContent>
-                    <li style={{borderBottom:"1px solid #959595"}}>수정</li>
-                    <li >삭제</li>
-                  </DotContent>
-                </DotContainer>
-              ):<></>}
+    <FolderContainer><FolderYellowIc/>{folderState.opendFolder}<DeleteIc onClick={deleteNowFolder}/></FolderContainer>
     <></>
     {/* 
     //옆에는 수정, 삭제 버튼
