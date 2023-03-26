e3b987cf77080526fee56feda1204be572c08b41
lj05117 <lj05117@naver.com>
2023-03-27 07:23:06 +0900

    feat: 모달창 지도에 가려지지 않게 하기
    Former-commit-id: 55d954699a67d91082980052901c600915d224eb

--- frontend/src/pages/MyCoursePage.tsx
+++ frontend/src/pages/MyCoursePage.tsx
@@ -4,8 +4,11 @@ import MyCourseSideBar from "../components/course/sideBar/index"
 import {FolderState} from "../recoil/CourseFolderState"
 import { useRecoilState } from "recoil";
 import {GuideManStyle, GuideText} from "../components/course/map/styles"
+import { modalState } from "../recoil/ModalState";
+
 const MyCoursePage = () => {
   const [folderState, setFolderState] = useRecoilState(FolderState);
+  const [mdState, setModalState] = useRecoilState(modalState);
   return (
     <CourseContainer>
       <SideItem>
@@ -19,7 +22,9 @@ const MyCoursePage = () => {
       <MapItem>
         {/* 폴더 클릭 시 받아오는 위, 경도 중 첫째값을 recoil에 담아서 kakaomap 태그의 위, 경도 값으로 주기 */}
         {folderState.isOpen?
-        <KakaoMap lat="33.450701" lng="126.570667" ></KakaoMap>:
+        (mdState.isOpen?<></>:
+        <KakaoMap lat="33.450701" lng="126.570667"></KakaoMap>)
+        :
           <><GuideText>코스를 선택해주세요</GuideText>
           <GuideManStyle/></>
         }
