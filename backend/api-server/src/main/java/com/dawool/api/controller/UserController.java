65ee673b23eacfe2a45d985450a681a1134f1978
jun <ktf1406@naver.com>
2023-03-27 17:29:06 +0900

    feat : 내 코스 조회, 생성, 추가 메서드 생성
    Former-commit-id: d3e9e29b1501b0c3f80a97ad6ba582e79b8fd8d7

--- backend/api-server/src/main/java/com/dawool/api/controller/UserController.java
+++ backend/api-server/src/main/java/com/dawool/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.dawool.api.controller;
 
 
+import com.dawool.api.dto.user.MyCourseDetailReqDto;
+import com.dawool.api.dto.user.MyCourseResDto;
 import com.dawool.api.dto.user.ReissueTokenReqDto;
 import com.dawool.api.dto.user.ReissueTokenResDto;
 import com.dawool.api.dto.user.TokenResDto;
@@ -9,12 +11,17 @@ import com.dawool.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 회원 controller
@@ -64,5 +71,48 @@ public class UserController {
     public ResponseEntity<?> survey(@RequestBody SurveyReqDto survey){
         userService.survey(survey);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+
+    /**
+     * 내 코스 조회
+     * 
+     * @return
+     */
+    @GetMapping("/my-course")
+    public ResponseEntity<?> getMyCourse() {
+        List<MyCourseResDto> mycourseList = userService.getMyCourse();
+        return ResponseEntity.ok(mycourseList);
+    }
+
+    /**
+     * 내 코스 폴더 추가
+     * 
+     * @param courseName
+     * @return
+     */
+    @PostMapping("/my-course")
+    public ResponseEntity<?> createCourse(@RequestBody String courseName) {
+        userService.createCourse(courseName);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+
+    @PostMapping("/my-course/{courseId}")
+    public ResponseEntity<?> addSpotToCourse(@PathVariable String courseId, @RequestBody MyCourseDetailReqDto myCourse) {
+        HttpStatus httpStatus = userService.addSpotToCourse(courseId, myCourse);
+        return ResponseEntity.status(httpStatus).body(httpStatus.getReasonPhrase());
+    }
+
+    @PutMapping("/my-course/{courseId}")
+    public ResponseEntity<?> modifyCourse(@PathVariable String courseId, @RequestBody String a) {
+
+        return null;
+    }
+
+    @DeleteMapping("/my-course/{courseId}")
+    public ResponseEntity<?> deleteCourse(@PathVariable String courseId) {
+
+        return null;
     }
 }
