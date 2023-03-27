220b2448c555351b6359d46ffdc37b7837d9f899
jungeun <jegarnet23@gmail.com>
2023-03-27 16:56:04 +0900

    feat: 북마크 Entity 생성
    Former-commit-id: 5c70772879483a3b926ea244ec40a7369394642a

---
+++ backend/api-server/src/main/java/com/dawool/api/entity/Heart.java
@@ -0,0 +1,29 @@
+package com.dawool.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * 북마크 관련 Entity
+ *
+ * @author 김정은
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "heart")
+public class Heart {
+
+    @Id
+    private String id;
+    private String userid;
+    private int contentid;
+}
