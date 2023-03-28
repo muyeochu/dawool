22f1bc644958a2205cff5ca4157ae84b39e3402e
jun <ktf1406@naver.com>
2023-03-28 15:30:00 +0900

    feat : 내 코스 기능 생성
    Former-commit-id: ff64d44038df3898c59cf049c6079a0f2c336ef0

--- backend/api-server/src/main/java/com/dawool/api/service/UserService.java
+++ backend/api-server/src/main/java/com/dawool/api/service/UserService.java
@@ -1,7 +1,8 @@
 package com.dawool.api.service;
 
-import com.dawool.api.dto.user.MyCourseDetailReqDto;
-import com.dawool.api.dto.user.MyCourseResDto;
+import com.dawool.api.dto.user.MyCourseDetailDto;
+import com.dawool.api.dto.user.MyCourseDto;
+import com.dawool.api.dto.user.MyCourseListDto;
 import com.dawool.api.dto.user.ReissueTokenReqDto;
 import com.dawool.api.dto.user.ReissueTokenResDto;
 import com.dawool.api.dto.user.TokenResDto;
@@ -225,22 +226,31 @@ public class UserService {
 
     }
 
-    public List<MyCourseResDto> getMyCourse() {
+    /**
+     * 코스 조회
+     *
+     * @return
+     */
+    public List<MyCourseListDto> getMyCourse() {
         String userId = getLoginUser();
 
         List<Course> courseList = courseRepository.findByUserid(userId);
 
-        List<MyCourseResDto> myCourseList = new ArrayList<>();
+        List<MyCourseListDto> myCourseList = new ArrayList<>();
 
         for (Course course : courseList) {
-            MyCourseResDto myCourseResDto = new MyCourseResDto().of(course);
+            MyCourseListDto myCourseResDto = new MyCourseListDto().of(course);
             myCourseList.add(myCourseResDto);
         }
 
         return myCourseList;
     }
 
-
+    /**
+     * 폴더 생성
+     * 
+     * @param courseName
+     */
     public void createCourse(String courseName) {
         String userId = getLoginUser();
         Course course = new Course();
@@ -253,23 +263,91 @@ public class UserService {
         courseRepository.save(course);
     }
 
-    public HttpStatus addSpotToCourse(String courseId, MyCourseDetailReqDto myCourse) {
+    /**
+     * 코스 상세 조회
+     * 
+     * @param courseId
+     * @return
+     */
+    public MyCourseDto detailCourse(String courseId) {
+        Course course = courseRepository.findById(courseId).orElseThrow();
+        MyCourseDto myCourse = new MyCourseDto().of(course);
+        return myCourse;
+    }
+
+    /**
+     * 코스에 여행지 추가
+     *
+     * @param courseId
+     * @param myCourse
+     * @return
+     */
+    public HttpStatus addSpotToCourse(String courseId, MyCourseDetailDto myCourse) {
         String userId = getLoginUser();
         Course course = courseRepository.findById(courseId).orElseThrow();
         if (!course.getUserid().equals(userId)) {
             return HttpStatus.FORBIDDEN;
         }
 
-        Spot spot = myCourse.of(myCourse);
-        course.getSpots().add(spot);
+        course.getSpots().add(new Spot().of(myCourse));
 
         courseRepository.save(course);
 
         return HttpStatus.OK;
     }
+    // 차후 수정 내용
+//    public HttpStatus modifyCourse(String courseId, String memo) {
+//        String userId = getLoginUser();
+//        Course course = courseRepository.findById(courseId).orElseThrow();
+//        if (!course.getUserid().equals(userId)) {
+//           return HttpStatus.FORBIDDEN;
+//        }
+//        JSONObject jsonObject = new JSONObject(memo);
+//        course.setMemo(jsonObject.getString("memo"));
+//
+//        courseRepository.save(course);
+//        return HttpStatus.OK;
+//    }
 
-    public void modifyCourse() {
+    /**
+     * 코스 삭제
+     *
+     * @param courseId
+     * @return
+     */
+    public HttpStatus deleteCourse(String courseId) {
         String userId = getLoginUser();
+        Course course = courseRepository.findById(courseId).orElseThrow();
 
+        if (!course.getUserid().equals(userId)) {
+           return HttpStatus.FORBIDDEN;
+        }
+
+        courseRepository.deleteById(courseId);
+        return HttpStatus.OK;
+    }
+
+    /**
+     * 여행지 삭제
+     *
+     * @param courseId
+     * @param spotId
+     * @return
+     */
+    public HttpStatus deleteSpot(String courseId, int spotId) {
+        String userId = getLoginUser();
+        Course course = courseRepository.findById(courseId).orElseThrow();
+
+        if (!course.getUserid().equals(userId)) {
+           return HttpStatus.FORBIDDEN;
+        }
+
+        List<Spot> spots = course.getSpots();
+        spots.remove(spotId);
+        course.setSpots(spots);
+
+        courseRepository.save(course);
+
+        return HttpStatus.OK;
     }
 }
