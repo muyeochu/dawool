35aedd23b1482f48d536da6a342d55249c973aa2
jun <ktf1406@naver.com>
2023-03-28 15:29:40 +0900

    feat : Entity 수정
    Former-commit-id: caefb43ea667c2eccc42a339de9b6174d395e717

--- backend/api-server/src/main/java/com/dawool/api/entity/Spot.java
+++ backend/api-server/src/main/java/com/dawool/api/entity/Spot.java
@@ -1,11 +1,11 @@
 package com.dawool.api.entity;
 
+import com.dawool.api.dto.user.MyCourseDetailDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
@@ -14,7 +14,24 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 public class Spot {
     private int contentid;
+    private int contenttypeid;
     private String title;
-    private float mapx;
-    private float mapy;
+    private double mapx;
+    private double mapy;
+
+    /**
+     * Dto -> Entity
+     *
+     * @param myCourseDetailDto
+     * @return
+     */
+    public Spot of(MyCourseDetailDto myCourseDetailDto) {
+        return Spot.builder()
+                .contentid(myCourseDetailDto.getContentId())
+                .contenttypeid(myCourseDetailDto.getContentTypeId())
+                .title(myCourseDetailDto.getTitle())
+                .mapx(Double.parseDouble(myCourseDetailDto.getMapX()))
+                .mapy(Double.parseDouble(myCourseDetailDto.getMapY()))
+                .build();
+    }
 }
