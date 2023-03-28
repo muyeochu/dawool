897ba90b60d52facdb74eebedd8ea7826090a34d
lj05117 <lj05117@naver.com>
2023-03-28 10:51:36 +0900

    test: 사용자 정보 반영 테스트
    Former-commit-id: 727b50806a73e2cb0588ab4bd1b020427bcdf23c

--- frontend/src/pages/MyCoursePage.tsx
+++ frontend/src/pages/MyCoursePage.tsx
@@ -5,10 +5,12 @@ import {FolderState} from "../recoil/CourseFolderState"
 import { useRecoilState } from "recoil";
 import {GuideManStyle, GuideText} from "../components/course/map/styles"
 import { modalState } from "../recoil/ModalState";
-
+import { userState } from "../recoil/UserState";
 const MyCoursePage = () => {
   const [folderState, setFolderState] = useRecoilState(FolderState);
   const [mdState, setModalState] = useRecoilState(modalState);
+  const [usState, setUsState] = useRecoilState(userState);
+  console.log(usState);
   return (
     <CourseContainer>
       <SideItem>
@@ -22,11 +24,14 @@ const MyCoursePage = () => {
       <MapItem>
         {/* 폴더 클릭 시 받아오는 위, 경도 중 첫째값을 recoil에 담아서 kakaomap 태그의 위, 경도 값으로 주기 */}
         {folderState.isOpen?
-        (mdState.isOpen?<></>:
-        <KakaoMap lat="33.450701" lng="126.570667"></KakaoMap>)
-        :
-          <><GuideText>코스를 선택해주세요</GuideText>
-          <GuideManStyle/></>
+          (mdState.isOpen?
+            <></>:
+          <KakaoMap lat="33.450701" lng="126.570667"></KakaoMap>)
+          :
+          <>
+            <GuideText>코스를 선택해주세요</GuideText>
+            <GuideManStyle/>
+          </>
         }
       </MapItem>
     </CourseContainer>
