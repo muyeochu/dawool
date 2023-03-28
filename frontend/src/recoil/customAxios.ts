744d363bcd41ec707058e53fcf13aa8bfed40a82
lj05117 <lj05117@naver.com>
2023-03-28 10:52:08 +0900

    feat: 헤더로 사용자 정보 보내는 axios 코드 생성
    Former-commit-id: 552a3a2260401ce990d7aabf5d0ab170e94190a0

--- frontend/src/recoil/customAxios.ts
+++ frontend/src/recoil/customAxios.ts
@@ -7,3 +7,12 @@ export const customAxios: AxiosInstance = axios.create({
     "Content-Type": "application/json;charset=UTF-8",
   },
 });
+
+  export const customAxios2: AxiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL + "/api/", // 서버의 기본 URL
+    headers: {
+      "Authorization" : "Bearer "+localStorage.getItem("token"),
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    }
+  )
