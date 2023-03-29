66fa6b98567be10c7aacd84ce0931f5cb95d7546
jun <ktf1406@naver.com>
2023-03-29 17:45:43 +0900

    feat : EntryPoint 추가
    Former-commit-id: bc0c62166ace55e5287d4a0c0a74e8fe0a375888

---
+++ backend/api-server/src/main/java/com/dawool/api/config/SecurityAuthenticationEntryPoint.java
@@ -0,0 +1,16 @@
+package com.dawool.api.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendRedirect("http://j8d105.p.ssafy.io:3000");
+    }
+}
