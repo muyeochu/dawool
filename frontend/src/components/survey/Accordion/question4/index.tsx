f0eab9b09b752ca12daaf82419b317eb7134a9a3
yyyrin <qsc130@gmail.com>
2023-03-30 07:21:06 +0900

    feat: 불필요한 코드 삭제
    Former-commit-id: 90d2924107df69a2324c9fb3301dd61db6aaa87f

--- frontend/src/components/survey/Accordion/question4/index.tsx
+++ frontend/src/components/survey/Accordion/question4/index.tsx
@@ -21,27 +21,11 @@ const FourthQuestion = () => {
 
   // 현재 선택된 radio button value
   const [selectedValue, setSelectedValue] = useRecoilState(fourthState);
-  const [isOpen, setIsOpen] = useState(false); // accordion이 열린 상태를 저장하는 state
   // 선택한 값을 selectedValue로 설정
   function radioGroupHandler(event: React.ChangeEvent<HTMLInputElement>) {
     setSelectedValue(event.target.value);
   }
-
-  // selectedValue가 변경될 때마다 실행
-  useEffect(() => {
-    if (isOpen) {
-      // accordion이 열린 경우에만 실행
-      const radioGroup = document.getElementsByName("button-types");
-      for (let i = 0; i < radioGroup.length; i++) {
-        const radio = radioGroup[i] as HTMLInputElement;
-        if (radio.value === selectedValue) {
-          radio.checked = true; // 선택한 값에 해당하는 라디오 버튼을 체크한 상태로 변경
-          break;
-        }
-      }
-    }
-    console.log(selectedValue)
-  }, [isOpen, selectedValue]);
+  // console.log(selectedValue)
 
   return (
     <RadioButtonContainer>
