8348777d5700ab2732a61fa6cc5919a0e1d9ba80
lj05117 <lj05117@naver.com>
2023-03-29 12:39:14 +0900

    feat: 내 코스 상세조회 통신 확인
    Former-commit-id: d622d0bb806620b6773b811a7d195a63dbbe0e70

--- frontend/src/components/course/sideBar/folderList/openFolder/courseList.tsx
+++ frontend/src/components/course/sideBar/folderList/openFolder/courseList.tsx
@@ -4,36 +4,83 @@ import { FolderState } from "../../../../../recoil/CourseFolderState";
 import { useRecoilState,selector, useRecoilValue } from "recoil";
 import { getCourseListData } from "../../../../../recoil/CourseListSelectors";
 import { ListType } from "../../../../../types/courseListTypes";
+import { customAxios2 } from "../../../../../recoil/customAxios";
+import { createTaggedTemplate } from "typescript";
+// import { FolderContainer } from "../styles";
+import ReactDOM from 'react-dom';
+import { AccomodationIc, CourseContainer, EntertainmentIc, RestaurantIc } from "./styles";
 
 export const CourseList = ()=>{
     const [folderState] = useRecoilState(FolderState);
     const Id = Number(folderState.courseId);
     //파일 리스트 불러오기
     //메모는 어디넣지?
-    const CourseListSelector = selector<ListType[]>({
-        key:"CourseListSelector",
-        get : async ()=>{
-            try{
-                const courseList = await getCourseListData(Id);
-                return courseList;
-            }catch(err){
-                throw err;
-            }
-        },
-    });
-    let courseLists = useRecoilValue<ListType[]>(CourseListSelector);
+    // const CourseListSelector = selector<ListType[]>({
+    //     key:"CourseListSelector",
+    //     get : async ()=>{
+    //         try{
+    //             const courseList = await getCourseListData(Id);
+    //             return courseList;
+    //         }catch(err){
+    //             throw err;
+    //         }
+    //     },
+    // });
+    // let courseLists = useRecoilValue<ListType[]>(CourseListSelector);
+    
+    const getCourseFileData = ():Promise<ListType[]>=>
+    customAxios2.get(`course/${folderState.courseId}`)
+    .then((res)=>{
+        console.log(res);
+        return res.data;
+    }).catch((err)=>{
+        console.log(err);
+    })
+    async function myFunction(){
+        try{
+            let courseList = await getCourseFileData();
+            return courseList;
+        }catch(err){
+            throw err;
+        }
+    }
+
+function createTag(courseList:any){
+    const div = document.getElementById("myCourseInfolder");
+    const div2 = document.createElement("div");
+    console.log(courseList)
+    for(let i=0;i<courseList["course"].length;i++){
+        const course = courseList["course"][i]
+        const element =()=><CourseContainer>{course.title}{course.contentTypeId===32?<AccomodationIc/>:course.contentTypeId===39?<RestaurantIc/>:<EntertainmentIc/>}</CourseContainer>
+        const csContainer = document.createElement("div");
+        ReactDOM.render(element(),csContainer);
+        div2.appendChild(csContainer);
+    }
+    div ?.appendChild(div2);
+    return div;
+}
+
+
+    myFunction().then(courseList=>{
+        createTag(courseList);
+    })  
+    
+    
+    
+    
+    
     // useEffect(()=>{
         
     // },[]);
     return(
-     <div> 
-        {
+     <div id = "myCourseInfolder"> 
+        {/* {
             courseLists.map((item)=>(
                 <>
                 {console.log(item)}
                 </>
             ))
-        }
+        } */}
      </div>   
     )
 }
