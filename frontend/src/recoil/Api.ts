c49ec470d160360c1d519dabfd47e74f08e4c14b
jungeun <jegarnet23@gmail.com>
2023-03-29 17:11:49 +0900

    feat: 북마크 리스트 API 구현
    Former-commit-id: 70dd5007987f0f298d89e3b05d03b03bbb23610e

--- frontend/src/recoil/Api.ts
+++ frontend/src/recoil/Api.ts
@@ -40,6 +40,7 @@ export const getRecEtcApi = async (recentContentId: number) =>
     }
   );
 
+// 북마크 저장 및 해제
 export const bookmarkApi = async (info: any) =>
   await customAxios2.post(
     `location/bookmark`,
@@ -58,9 +59,14 @@ export const bookmarkApi = async (info: any) =>
     },
     {
       withCredentials: true,
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
     }
   );
+
+// 북마크 목록 불러오기
+export const getBookmarkListApi = async (page: number, size: number) =>
+  await customAxios.get(`location/bookmark?page=${page}&size=${size}`, {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
