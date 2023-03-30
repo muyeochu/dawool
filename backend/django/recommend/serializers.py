61a67eacb66e9d18bd38eab364e34b1676140d14
heejucherich <gmlwn1954@gmail.com>
2023-03-30 13:20:38 +0900

    chore: 추천 응답값 수정
    Former-commit-id: 1b6d5998d2d014d71ce645cb1f9495db3853e234

--- backend/django/recommend/serializers.py
+++ backend/django/recommend/serializers.py
@@ -3,15 +3,25 @@ from .models import RecommendTour
 
 
 class RecommendSerializer(serializers.ModelSerializer):
+    contentTypeId = serializers.IntegerField(source='contenttypeid')
+    contentId = serializers.IntegerField(source='contentid')
+    imageUrl= serializers.CharField(source='firstimage')
+    areaCode = serializers.IntegerField(source='areacode')
+
     class Meta:
         model = RecommendTour
-        fields = [ '_id','title','keyword_result','searchcount','areacode','contentid','contenttypeid','addr1','deaf','visual_impaired','infant','mobility_weak','old','firstimage','location']
+        fields = [ '_id','title','keyword_result','searchcount','areaCode','contentId','contentTypeId','addr1','deaf','visual_impaired','infant','mobility_weak','old','imageUrl','location']
 
 
 
 class PopularTourSerializer(serializers.ModelSerializer):
+    contentTypeId = serializers.IntegerField(source='contenttypeid')
+    contentId = serializers.IntegerField(source='contentid')
+    imageUrl= serializers.CharField(source='firstimage')
+    areaCode = serializers.IntegerField(source='areacode')
+
     class Meta:
         model = RecommendTour
-        fields = [ 'title','contentid','contenttypeid','firstimage','areacode']
+        fields = [ 'title','contentId','contentTypeId','imageUrl','areaCode']
 
 
