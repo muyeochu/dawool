ab749e5a6f01b091b4e5d6dde3320180281dd05b
jun <ktf1406@naver.com>
2023-03-30 17:32:42 +0900

    feat : 인증 에러 처리
    Former-commit-id: 659acb3f33b0ad2bff6cdaafeddf6a713ddc2606

--- backend/api-server/src/main/java/com/dawool/api/config/SecurityAuthenticationEntryPoint.java
+++ backend/api-server/src/main/java/com/dawool/api/config/SecurityAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.dawool.api.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -11,6 +12,7 @@ import java.io.IOException;
 public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendRedirect("http://j8d105.p.ssafy.io:3000");
+        System.out.println("에에에에에엥에에에에엥에에에에에엥   에에에에에에엥에에엥 에러발생! 에러발생! 에러발생! 에에에에에엥에에에엥에에엥");
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
     }
 }
