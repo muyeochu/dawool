e6452e6cc7d554fd301e8f38b92953d2cdcbf78c
lj05117 <lj05117@naver.com>
2023-04-02 12:21:12 +0900

    feat: 지도 전체 마커 띄우는 태그 설정
    Former-commit-id: 1c23634dd0ee7be12b37d36a4d1b45ffe8f356e5

--- frontend/src/pages/MyCoursePage.tsx
+++ frontend/src/pages/MyCoursePage.tsx
@@ -1,38 +1,64 @@
-import {CourseContainer, SideItem, SideHeader, MapItem, SideHeaderText, MapIcStyle} from "../components/course/styles"
+import {
+  CourseContainer,
+  SideItem,
+  SideHeader,
+  MapItem,
+  SideHeaderText,
+  MapIcStyle,
+} from "../components/course/styles";
 import KakaoMap from "../components/course/map";
-import MyCourseSideBar from "../components/course/sideBar/index"
-import {FolderState} from "../recoil/CourseFolderState"
+import MyCourseSideBar from "../components/course/sideBar/index";
+import { FolderState } from "../recoil/CourseFolderState";
 import { useRecoilState } from "recoil";
-import {GuideManStyle, GuideText} from "../components/course/map/styles"
+import { GuideManStyle, GuideText } from "../components/course/map/styles";
 import { modalState } from "../recoil/ModalState";
 import { userState } from "../recoil/UserState";
+import { clickedState } from "../recoil/ClickedCourse";
+import ClickedKakaoMap from "../components/course/map/ClickedMyCourseMap";
+import Markers from "../components/course/map/markers";
 const MyCoursePage = () => {
   const [folderState, setFolderState] = useRecoilState(FolderState);
   const [mdState, setModalState] = useRecoilState(modalState);
   const [usState, setUsState] = useRecoilState(userState);
-  console.log(usState);
+  const [clState, setClState] = useRecoilState(clickedState);
+  let mapX = "";
+  let mapY = "";
+  function getXY() {
+    let x = document.getElementById("클릭")?.getAttribute("mapX");
+    // if(x)
+    let y = document.getElementById("클릭")?.getAttribute("mapY");
+    // console.log(mapXY)
+  }
+  getXY();
+  // if(clState.isClicked){
+  //   mapX =clState.mapX.toString();
+  //   mapY = clState.mapY.toString();
+  // }
   return (
     <CourseContainer>
       <SideItem>
-          <SideHeader>
-            <SideHeaderText>
-              <MapIcStyle /> 내 코스 관리
-            </SideHeaderText>
-          </SideHeader>
-          <MyCourseSideBar/>
+        <SideHeader>
+          <SideHeaderText>
+            <MapIcStyle /> 내 코스 관리
+          </SideHeaderText>
+        </SideHeader>
+        <MyCourseSideBar />
       </SideItem>
       <MapItem>
         {/* 폴더 클릭 시 받아오는 위, 경도 중 첫째값을 recoil에 담아서 kakaomap 태그의 위, 경도 값으로 주기 */}
-        {folderState.isOpen?
-          (mdState.isOpen?
-            <></>:
-          <KakaoMap lat="33.450701" lng="126.570667"></KakaoMap>)
-          :
+        {folderState.isOpen ? (
+          mdState.isOpen ? (
+            <></>
+          ) : (
+            // 클릭 시 그 위치로 지도 옮기는 기능 설정하기
+            <Markers></Markers>
+          )
+        ) : (
           <>
             <GuideText>코스를 선택해주세요</GuideText>
-            <GuideManStyle/>
+            <GuideManStyle />
           </>
-        }
+        )}
       </MapItem>
     </CourseContainer>
   );
