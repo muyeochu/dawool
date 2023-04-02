5d68f273c1bf06ab0d001ceb375b039d6deaa10c
lj05117 <lj05117@naver.com>
2023-04-02 21:53:32 +0900

    feat: 클릭한 장소만 마커 바꾸고 줌인
    Former-commit-id: 0d36372f8f2fd29b07db940b891d323c3987ac08

--- frontend/src/components/course/map/ClickedMyCourseMap.tsx
+++ frontend/src/components/course/map/ClickedMyCourseMap.tsx
@@ -1,5 +1,10 @@
 import { useEffect } from "react";
 import styled from "styled-components";
+import { ListType } from "../../../types/courseListTypes";
+import { customAxios2 } from "../../../recoil/customAxios";
+import { useRecoilState } from "recoil";
+import { FolderState } from "../../../recoil/CourseFolderState";
+import { clickedState } from "../../../recoil/ClickedCourse";
 declare global {
   //무슨 설정인지 잘은 모르겠음
   interface Window {
@@ -16,6 +21,9 @@ type MapProps = {
   lng: String;
 };
 const ClickedKakaoMap = () => {
+  const [folderState, setFolderState] = useRecoilState(FolderState);
+  const [clState, setClState] = useRecoilState(clickedState);
+  console.log(clState.mapX);
   const key = process.env.REACT_APP_KAKAOMAP_API_KEY; //key 받아오기
   useEffect(() => {
     // window.onload=()=>{
@@ -27,67 +35,88 @@ const ClickedKakaoMap = () => {
 
     const onLoadKakaoMap = () => {
       window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById("clickedMap");
-        let markerPosition = new window.kakao.maps.LatLng();
-        //   Number(lat),
-        //   Number(lng) //받아온 위도, 경도를 숫자로 변환해서 마커 위치로 설정
-        const mapOption = {
-          center: markerPosition, // 지도의 중심좌표. 컴포넌트 불러올 때 좌표를 임의로 설정해서 쓸 수 있게 설정해놨슴다.
-          level: 3, //지도 크기?
-        };
-        let map = new window.kakao.maps.Map(mapContainer, mapOption); //지도를 불러오는 변수
-        console.log(map);
+        const getCourseFileData = (): Promise<ListType[]> =>
+          customAxios2
+            .get(`user/my-course/${folderState.courseId}`)
+            .then((res) => {
+              console.log(res);
+              return res.data;
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        async function myFunction() {
+          try {
+            let courseList: any = await getCourseFileData();
+            return courseList;
+          } catch (err) {
+            throw err;
+          }
+        }
+        myFunction().then((courseList) => {
+          window.xyList = [];
+          window.len = courseList["course"].length;
+          if (window.len === 0) return;
+          var mapContainer = document.getElementById("clickedMap"), // 지도를 표시할 div
+            mapOption = {
+              center: new window.kakao.maps.LatLng(
+                Number(clState.mapX),
+                Number(clState.mapY)
+              ), // 지도의 중심좌표
+              level: 5, // 지도의 확대 레벨
+            };
 
-        // 마커를 생성합니다
-        let marker = new window.kakao.maps.Marker({
-          position: markerPosition,
+          let map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+          // 마커가 표시될 위치입니다
+          let positions: any = [];
+          var imageSrc =
+            "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+          for (let i = 0; i < window.len; i++) {
+            var imageSize = new window.kakao.maps.Size(24, 35);
+            var markerImage = new window.kakao.maps.MarkerImage(
+              imageSrc,
+              imageSize
+            );
+
+            const mapX = courseList["course"][i]["mapY"];
+            const mapY = courseList["course"][i]["mapX"];
+            positions.push({
+              latlng: new window.kakao.maps.LatLng(Number(mapX), Number(mapY)),
+            });
+            if (
+              Number(mapX) === Number(clState.mapX) &&
+              Number(mapY) === Number(clState.mapY)
+            ) {
+              var marker = new window.kakao.maps.Marker({
+                map: map, // 마커를 표시할 지도
+                position: positions[i].latlng, // 마커를 표시할 위치
+                title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                image: markerImage, // 마커 이미지
+              });
+            } else {
+              var marker = new window.kakao.maps.Marker({
+                map: map, // 마커를 표시할 지도
+                position: positions[i].latlng, // 마커를 표시할 위치
+                title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                //   image: markerImage, // 마커 이미지
+              });
+            }
+            marker.setMap(map);
+          }
         });
-        marker.setMap(map); //지도에 마커 찍기
-
-        //                 // 마커를 표시할 위치와 title 객체 배열입니다
-        //마커 여러개 찍는 용 코드. 리코일에서 폴더 열려있으면 이 함수 실행시키기.
-        // var positions = [
-        //     {
-        //         title: '카카오',
-        //         latlng: new kakao.maps.LatLng(33.450705, 126.570677)
-        //     },
-        //     {
-        //         title: '생태연못',
-        //         latlng: new kakao.maps.LatLng(33.450936, 126.569477)
-        //     },
-        //     {
-        //         title: '텃밭',
-        //         latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-        //     },
-        //     {
-        //         title: '근린공원',
-        //         latlng: new kakao.maps.LatLng(33.451393, 126.570738)
-        //     }
-        // ];
-        // 마커 이미지의 이미지 주소입니다
-        // var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-
-        // for (var i = 0; i < positions.length; i ++) {
-
-        //     // 마커 이미지의 이미지 크기 입니다
-        //     var imageSize = new kakao.maps.Size(24, 35);
-
-        //     // 마커 이미지를 생성합니다
-        //     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-        //     // 마커를 생성합니다
-        //     var marker = new kakao.maps.Marker({
-        //         map: map, // 마커를 표시할 지도
-        //         position: positions[i].latlng, // 마커를 표시할 위치
-        //         title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        //         image : markerImage // 마커 이미지
-        //     });}
-        // }
       });
     };
+
     mapScript.addEventListener("load", onLoadKakaoMap);
+
     return () => {
-      // mapScript.removeEventListener("load",onLoadKakaoMap);
+      // mapScript.removeEventListener("load", onLoadKakaoMap);
+      setClState({
+        isClicked: false,
+        mapX: "",
+        mapY: "",
+      });
     };
     // }
   }, []);
