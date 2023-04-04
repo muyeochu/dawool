6e1ce4c4096aa7b37d0c75df522df272e8cb2723
jun <ktf1406@naver.com>
2023-04-04 10:16:25 +0900

    fix : 로그로 변경
    Former-commit-id: a630ffb3d821448618209c673ad6629c0d25c0e0

--- backend/api-server/src/main/java/com/dawool/api/config/SecurityAuthenticationEntryPoint.java
+++ backend/api-server/src/main/java/com/dawool/api/config/SecurityAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.dawool.api.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -9,10 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        System.out.println("에에에에에엥에에에에엥에에에에에엥   에에에에에에엥에에엥 에러발생! 에러발생! 에러발생! 에에에에에엥에에에엥에에엥");
+        log.error("에에에에에엥에에에에엥에에에에에엥   에에에에에에엥에에엥 에러발생! 에러발생! 에러발생! 에에에에에엥에에에엥에에엥");
         response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
     }
 }
