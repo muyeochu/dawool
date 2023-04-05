734e9c69379aba8a2a1e9b8e6071e0bc18cf16e5
jungeun <jegarnet23@gmail.com>
2023-04-05 16:22:53 +0900

    refac: 에러 로그 수정
    Former-commit-id: 75c4b639c1b5d19020172607e7c33f21ee060bec

--- backend/api-server/src/main/java/com/dawool/api/config/SecurityAuthenticationEntryPoint.java
+++ backend/api-server/src/main/java/com/dawool/api/config/SecurityAuthenticationEntryPoint.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        log.error("에에에에에엥에에에에엥에에에에에엥   에에에에에에엥에에엥 에러발생! 에러발생! 에러발생! 에에에에에엥에에에엥에에엥");
+        log.error("**********Error**********");
         response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
     }
 }
